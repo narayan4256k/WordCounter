d21710e4a48db3e5c6f2f15e18757127ec7cebea
Narayan <88489426+narayan4256k@users.noreply.github.com>
2025-07-04 19:22:58 +0530

    changed github link text color

--- src/components/About.js
+++ src/components/About.js
@@ -52,7 +52,7 @@ export default function About(props) {
           </h2>
           <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
             <div className="accordion-body">
-              Made with ❤️ by Narayan — a chill dev who vibes with clean UI, good UX, and functional code. Follow me on <a href="https://github.com/narayan4256k" target="_blank" rel="noopener noreferrer">GitHub</a> for more cool stuff.
+              Made with ❤️ by Narayan — a chill dev who vibes with clean UI, good UX, and functional code. Follow me on <a href="https://github.com/narayan4256k" target="_blank" rel="noopener noreferrer" style={{color: props.mode ==='dark' ? '#81f3b4':'rgb(255 0 0)', textDecoration:'none'}}>GitHub</a> for more cool stuff.
             </div>
           </div>
         </div>
