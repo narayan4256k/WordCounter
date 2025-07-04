96b73bd3b0ea6f4a3fbbc1ad5079d279bd9c7bae
Narayan <88489426+narayan4256k@users.noreply.github.com>
2025-07-04 17:54:47 +0530

    Initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 //import PropTypes from "prop-types";
 
 export default function Navbar(props) {
@@ -8,10 +8,10 @@ export default function Navbar(props) {
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <img src="android-chrome-512x512.png" alt="logo" width="50" height="50" className="d-inline-block align-text-top mx-2"/>
       <div className="container-fluid">
-        {/*<Link className="navbar-brand" to="/">
+       <Link className="navbar-brand" to="/">
           {props.title}
-        </Link>*/}
-        <a href="/" className="navbar-brand">{props.title}</a>
+        </Link>
+         {/*<a href="/" className="navbar-brand">{props.title}</a>*/}
         <button
           className="navbar-toggler"
           type="button"
@@ -25,7 +25,7 @@ export default function Navbar(props) {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            {/*<li className="nav-item">
+            <li className="nav-item">
               <Link className="nav-link " aria-current="page" to="/">
                 Home
               </Link>
@@ -34,10 +34,10 @@ export default function Navbar(props) {
               <Link className="nav-link " to="/about">
                 {props.aboutText}
               </Link>
-            </li>*/}
-            <li className="nav-items">
+            </li>
+             {/*<li className="nav-items">
               <a className='navbar-brand' href='#'>Home</a>
-              </li>
+              </li>*/}
           </ul>
           <form className="d-flex" role="search">
             <input
